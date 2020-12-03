490a7e2a65c2cb06a7dfc0e68f46dd6826a178e0
Kevin Retzke <retzkek@gmail.com>
2020-12-03 10:54:51 -0600

    day2: just pipes

--- day02.jl
+++ day02.jl
@@ -27,10 +27,12 @@ isvalid2((b, e, c, p)) = isvalid2(b,e,c,p)
 function main(fn)
     #println(sum(map(x->fn(parseline(x)...), eachline("input/day02.txt"))))
     # That works, but maybe pipes and function composition is clearer?
-    eachline("input/day02.txt") .|> fn ∘ parseline |> sum |> println
+    #eachline("input/day02.txt") .|> fn ∘ parseline |> sum |> println
     # I don't know, too much syntax maybe. Handy for rapid development and
     # debugging though, just build the pipe one part at a time. To debug a step
     # just comment out the rest of the pipe.
+    # The composition interrupts the flow, how about just pipes?
+    eachline("input/day02.txt") .|> parseline .|> fn |> sum |> println
 end
 
 main(isvalid1)
