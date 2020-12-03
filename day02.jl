767322a7898d98e100f58bdfdb48029b4fcfb433
Kevin Retzke <retzkek@gmail.com>
2020-12-03 10:46:18 -0600

    day2: rewrite main process with pipes
    May not be better, but it's more interesting for learning Julia.

--- day02.jl
+++ day02.jl
@@ -13,17 +13,24 @@ end
 @test pw"1-3 a: abcde" == (1,3,'a',"abcde")
 
 isvalid1(b, e, c, p) = b <= length(filter(x->x==c, p)) <= e
-@test isvalid1(pw"1-3 a: abcde"...)
-@test !isvalid1(pw"1-3 b: cdefg"...)
-@test isvalid1(pw"2-9 c: ccccccccc"...)
+isvalid1((b, e, c, p)) = isvalid1(b,e,c,p)
+@test isvalid1(pw"1-3 a: abcde")
+@test !isvalid1(pw"1-3 b: cdefg")
+@test isvalid1(pw"2-9 c: ccccccccc")
 
 isvalid2(b, e, c, p) = (p[b] == c) ⊻ (p[e] == c)
-@test isvalid2(pw"1-3 a: abcde"...)
-@test !isvalid2(pw"1-3 b: cdefg"...)
-@test !isvalid2(pw"2-9 c: ccccccccc"...)
+isvalid2((b, e, c, p)) = isvalid2(b,e,c,p)
+@test isvalid2(pw"1-3 a: abcde")
+@test !isvalid2(pw"1-3 b: cdefg")
+@test !isvalid2(pw"2-9 c: ccccccccc")
 
 function main(fn)
-    println(sum(map(x->fn(parseline(x)...), eachline("input/day02.txt"))))
+    #println(sum(map(x->fn(parseline(x)...), eachline("input/day02.txt"))))
+    # That works, but maybe pipes and function composition is clearer?
+    eachline("input/day02.txt") .|> fn ∘ parseline |> sum |> println
+    # I don't know, too much syntax maybe. Handy for rapid development and
+    # debugging though, just build the pipe one part at a time. To debug a step
+    # just comment out the rest of the pipe.
 end
 
 main(isvalid1)
