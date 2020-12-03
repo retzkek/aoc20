70aa05a753f940fd9e5882ab458e2e74365301bb
Kevin Retzke <retzkek@gmail.com>
2020-12-03 08:25:14 -0600

    day3: solve part 2 with julia

--- day03.jl
+++ day03.jl
@@ -14,15 +14,41 @@ testmap = b"""
     .#..#...#.#"""
 
 readmap(in) = collect(eachline(in))
-#println(readmap(IOBuffer(testmap)))
+@test readmap(IOBuffer(testmap)) == ["..##.......",
+                                     "#...#...#..",
+                                     ".#....#..#.",
+                                     "..#.#...#.#",
+                                     ".#...##..#.",
+                                     "..#.##.....",
+                                     ".#.#.#....#",
+                                     ".#........#",
+                                     "#.##...#...",
+                                     "#...##....#",
+                                     ".#..#...#.#"]
 
-# ok, this comprehension is getting a bit.. uncomprehensible :/
-trees(map, slope) =  sum([map[j][1+(slope*(j-1)%length(map[j]))] for j in 1:size(map,1)] .== '#')
-@test trees(readmap(IOBuffer(testmap)),3) == 7
+function trees(map, xstr, ystr)
+    i,j = 0,1
+    cnt = 0
+    while j <= size(map,1)
+        cnt += map[j][1+i] == '#'
+        i = (i + xstr)%length(map[j])
+        j += ystr
+    end
+    cnt
+end
+@testset "counting trees" begin
+    m = readmap(IOBuffer(testmap))
+    @test trees(m,3,1) == 7
+    @test trees(m,1,1) == 2
+    @test trees(m,5,1) == 3
+    @test trees(m,7,1) == 4
+    @test trees(m,1,2) == 2
+end
 
 function main()
     m = readmap("input/day03.txt")
-    println(trees(m,3))
+    println(trees(m,3,1))
+    println(reduce(*,[trees(m,x...) for x in [(1,1),(3,1),(5,1),(7,1),(1,2)]]))
 end
 
 main()
