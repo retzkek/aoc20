3562ed2ba93e8356def1fdd47186025e9f9b32ed
Kevin Retzke <retzkek@gmail.com>
2020-12-04 09:05:51 -0600

    day4: part 2 in julia

--- day04.jl
+++ day04.jl
@@ -51,7 +51,7 @@ function read_passports(in)
 end
 @test length(read_passports(IOBuffer(testdata))) == 4
 
-function valid(p)
+function hasfields(p)
     required = ("byr","iyr","eyr","hgt","hcl","ecl","pid")
     for f in required
         if !haskey(p,f)
@@ -60,11 +60,102 @@ function valid(p)
     end
     true
 end
+@test read_passports(IOBuffer(testdata)) .|> hasfields |> sum == 2
 
-@test read_passports(IOBuffer(testdata)) .|> valid |> sum == 2
+#    byr (Birth Year) - four digits; at least 1920 and at most 2002.
+#    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+#    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+#    hgt (Height) - a number followed by either cm or in:
+#        If cm, the number must be at least 150 and at most 193.
+#        If in, the number must be at least 59 and at most 76.
+#    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+#    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+#    pid (Passport ID) - a nine-digit number, including leading zeroes.
+#    cid (Country ID) - ignored, missing or not.
 
-function main()
-    read_passports("input/day04.txt") .|>  valid |> sum
+
+valid_byr(p) = 1920 <= parse(Int,p["byr"]) <= 2002
+valid_iyr(p) = 2010 <= parse(Int,p["iyr"]) <= 2020
+valid_eyr(p) = 2020 <= parse(Int,p["eyr"]) <= 2030
+
+function valid_hgt(p)
+    m = match(r"(\d+)(\w{2})", p["hgt"])
+    if isnothing(m) return false end
+    h = parse(Int, m[1])
+    if m[2] == "cm"
+        150 <= h <= 193
+    elseif m[2] == "in"
+        59 <= h <= 76
+    else
+        false
+    end
+end
+@testset "hgt" begin
+    @test valid_hgt(Dict("hgt"=>"60in"))
+    @test valid_hgt(Dict("hgt"=>"190cm"))
+    @test !valid_hgt(Dict("hgt"=>"190in"))
+    @test !valid_hgt(Dict("hgt"=>"190"))
 end
 
-println(main())
+valid_hcl(p) = !isnothing(match(r"^#[0-9a-f]{6}$",p["hcl"]))
+@testset "hcl" begin
+    @test valid_hcl(Dict("hcl"=>"#123abc"))
+    @test !valid_hcl(Dict("hcl"=>"#123abz"))
+    @test !valid_hcl(Dict("hcl"=>"123abc"))
+end
+
+valid_ecl(p) = p["ecl"] in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+@testset "ecl" begin
+    @test valid_ecl(Dict("ecl"=>"brn"))
+    @test !valid_ecl(Dict("ecl"=>"wat"))
+end
+
+valid_pid(p)= !isnothing(match(r"^\d{9}$",p["pid"]))
+@testset "pid" begin
+    @test valid_pid(Dict("pid"=>"000000001"))
+    @test !valid_pid(Dict("pid"=>"0123456789"))
+end
+
+valid(p) = hasfields(p) && valid_byr(p) && valid_iyr(p) && valid_eyr(p) &&
+    valid_hgt(p) && valid_hcl(p) && valid_ecl(p) && valid_pid(p)
+
+@testset "passports" begin
+    testinvalid = b"""
+        eyr:1972 cid:100
+        hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+        iyr:2019
+        hcl:#602927 eyr:1967 hgt:170cm
+        ecl:grn pid:012533040 byr:1946
+
+        hcl:dab227 iyr:2012
+        ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+        hgt:59cm ecl:zzz
+        eyr:2038 hcl:74454a iyr:2023
+        pid:3556412378 byr:2007
+        """
+    @test read_passports(IOBuffer(testinvalid)) .|> valid |> sum == 0
+    testvalid = b"""
+        pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+
+        eyr:2029 ecl:blu cid:129 byr:1989
+        iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+        hcl:#888785
+        hgt:164cm byr:2001 iyr:2015 cid:88
+        pid:545766238 ecl:hzl
+        eyr:2022
+
+        iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+        """
+    @test read_passports(IOBuffer(testvalid)) .|> valid |> sum == 4
+end
+
+function main(validator)
+    read_passports("input/day04.txt") .|>  validator |> sum
+end
+
+println(main(hasfields))
+println(main(valid))
