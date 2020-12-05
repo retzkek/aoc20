6a368a1e30cacf6ad4d08f841389beb6c3b8a43d
Kevin Retzke <retzkek@gmail.com>
2020-12-04 20:44:55 -0600

    day1: part 2 in common lisp
    could do better

--- day01.lisp
+++ day01.lisp
@@ -18,9 +18,19 @@
   (mapcar (lambda (x) (cons (+ (car x) (cdr x)) (* (car x) (cdr x))))
           (combinations ns)))
 
-(defun product-where-sum-is (ns sum)
+(defun sums-and-products-3 (ns)
+  "Return pairs of the sums and products of all three-element permutations of ns."
+  (loop for a in ns
+        append
+        (loop for b in (remove-if (lambda (x) (= x a)) ns)
+              append
+              (loop for c in (remove-if (lambda (x) (or (= x a) (= x b))) ns)
+                    collect (cons (+ a b c) (* a b c))))))
+
+(defun product-where-sum-is (ns k sum)
   (cdr (find-if (lambda (x) (= (car x) sum))
-                (sums-and-products ns))))
+                (cond ((= k 2) (sums-and-products ns))
+                      ((= k 3) (sums-and-products-3 ns))))))
 
 (defparameter *test-in* "1721
 979
@@ -32,8 +42,11 @@
 (defparameter *test-r* (with-input-from-string (is *test-in*)
                          (read-report is)))
 
-(test (= (product-where-sum-is *test-r* 2020) 514579))
+(test (= (product-where-sum-is *test-r* 2 2020) 514579))
+(test (= (product-where-sum-is *test-r* 3 2020) 241861950))
 
 (defun main ()
   (with-open-file (is "input/day01.txt")
-    (product-where-sum-is (read-report is) 2020)))
+    (let ((r (read-report is)))
+      (format t "~a~%" (product-where-sum-is r 2 2020))
+      (format t "~a~%" (product-where-sum-is r 3 2020)))))
